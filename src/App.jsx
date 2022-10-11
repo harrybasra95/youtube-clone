2d7078ecfc0cad166b819d6cdccba02140033d3a
Harnarinder Singh <harry.basra95@gmail.com>
2022-10-11 14:52:58 +0530

    Removed previous files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import './App.css';
-import SideBar from './Components/MainContent/SideBar/SideBar';
-import NavBar from './Components/Navbar/NavBar';
-import Content from './Components/MainContent/Content/Content';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <div className='Content'>
-        <SideBar></SideBar>
-        <Content></Content>
-      </div>
     </div>
   );
 }
--- src/Components/Sample.jsx
+++
@@ -1,8 +0,0 @@
-// import './SearchBar.css';
-// import logoImg from './logo.png';
-
-// const SearchBar = ()=>{
-
-// }
-
-// export default SearchBar; 
